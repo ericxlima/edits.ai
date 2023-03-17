dbf1ad1bad5779bb962398251aacf08d82eb82f4
Eric de Lima <eric.vinlima@gmail.com>
2023-03-17 05:04:50 -0300

    fix: fix bug to pytube path and refac code struct

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # RiveSong
+
+- Before run
+```py
+# add 
+if 'reelShelfRenderer' in video_details:
+    continue
+if "showingResultsForRenderer" in video_details:
+    continue
+# in below line 150 of pytube/contrib/search.py 
+```
+```py
+# In the file pytube/parser.py
+# change this line 
+[152]: func_regex = re.compile(r"function\([^)]+\)")
+# to this
+[152]: func_regex = re.compile(r"function\([^)]*\)")
+```
+```py
+# in pytube/cipher.py modify this line
+transform_plan_raw = find_object_from_startpoint(raw_code, match.span()[1] - 1)
+# for this
+transform_plan_raw = js
+```
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import time
 
 from dotenv import load_dotenv
 from pytube import Search
@@ -7,7 +8,23 @@ from pytube import Search
 import replicate
 
 
-def get_lyrics(music_artist: str, music_name: str, api_key: str) -> str:
+def download_music(music_artist: str, music_name: str) -> None:
+    try:
+        search_query = Search(music_artist + ' ' + music_name)
+        search_query = search_query.results[0]
+        music = search_query.streams.filter(only_audio=True).first()
+        downloaded_file = music.download(files_path)
+        base, _ = os.path.splitext(downloaded_file)
+        new_file = base + '.mp3'
+        os.rename(downloaded_file, new_file)
+        print(f">>> The music {music_name.upper()} by {music_artist.upper()} was downloaded.")
+    except Exception as err:
+        print(f'">>> Error while download the music {music_name.upper()} by {music_artist.upper()}.')
+        print(">>> ", err)
+
+
+def get_lyrics(music_artist: str, music_name: str, api_key: str, 
+               start_phrase:int=0, qtd_phrases:int=5) -> list:
     try:
         url = 'https://api.vagalume.com.br/search.php?art={0}&mus={1}&apikey={2}' \
             .format(music_artist, music_name, api_key)
@@ -15,70 +32,67 @@ def get_lyrics(music_artist: str, music_name: str, api_key: str) -> str:
 
         if response.status_code == 200:
             json_data = response.json()
+            if json_data['type'] == 'notfound':
+                print(f'>>> Error while get the lyrics of the music {music_name.upper()} by {music_artist.upper()}.')
+                return None
             music = json_data['mus'][0]['text']
             music = music.replace('\n\n', '\n')
+            music = music.split('\n')[start_phrase: start_phrase + qtd_phrases]
+            print(f'>>> Get the lyrics of the music {music_name.upper()} by {music_artist.upper()}.')
             return music
-            print(f'Get the lyrics of the music {music_name.upper()} by {music_artist.upper()}.')
-            # print(music)
         else:
-            print(f'Error while get the lyrics of the music {music_name.upper()} by {music_artist.upper()}.')
+            print(f'>>> Error while get the lyrics of the music {music_name.upper()} by {music_artist.upper()}.')
     except Exception as err:
-        print(f'Error while get the lyrics of the music {music_name.upper()} by {music_artist.upper()}.')
-        print(err)
+        print(f'>>> Error while get the lyrics of the music {music_name.upper()} by {music_artist.upper()}.')
+        print('>>> ', err)
 
 
-def download_music(music_artist: str, music_name: str) -> None:
+def generate_images(lyrics:list[str]) -> None:
     try:
-        search_query = Search(music_artist + ' ' + music_name)
-        search_query = search_query.results[0]
-        print('Downloading the music...', music)
-        music = search_query.streams.filter(only_audio=True).first()
-        downloaded_file = music.download(files_path)
-        base, _ = os.path.splitext(downloaded_file)
-        new_file = base + '.mp3'
-        os.rename(downloaded_file, new_file)
-        print(f"The music {music_name.upper()} by {music_artist.upper()} was downloaded.")
-    except Exception as err:
-        print(f'Error while download the music {music_name.upper()} by {music_artist.upper()}.')
-        print(err)
-
-
-def generate_images(lyrics:str) -> None:
-    try:
-        for idx, phrase in enumerate(lyrics.split('\n')):
+        for idx, phrase in enumerate(lyrics):
             inputs = {
                 'prompt': phrase,
                 'image_dimensions': "768x768",
                 'num_outputs': 1,
                 'num_inference_steps': 50,
                 'guidance_scale': 7.5,
-                'scheduler': "DPMSolverMultistep",
+                'scheduler': "PNDM",
+                'negative_prompt':'text,phrases,words,Graffiti',
             }
             output = version.predict(**inputs)
             img_data = requests.get(output[0]).content
             with open(f'{files_path}/{idx}.jpg', 'wb') as handler:
                 handler.write(img_data)
-            print(f'The image {idx}.jpg was generated.')
-        print('All images was generated.')
+            print(f'>>> The image {idx}.jpg was generated.')
+            time.sleep(2)
+        print('>>> All images was generated.')
     except Exception as err:
-        print(f'Error while generate the images.')
-        print(err)
+        print(f'>>> Error while generate the images.')
+        print('>>> ', err)
+        
+def create_video():
+    pass
 
 
 if __name__ == '__main__':
     
+    # User Settings
+    artist = "Compton's Most Wanted"
+    music = "Hood Took Me Under"
+    files_path = "temp/"
+    first_phrase = 0
+    qtd_phrases = 10
+    
+    # Environment Settings
     load_dotenv()
-    artist = 'Adelle'
-    music = 'Hello'
     vagalume_api_key = os.getenv("vagalumeKey")
     hugginface_api_key = os.getenv('hugginfaceKey')
     replicate_key = os.getenv('replicateKey')
-    files_path = "temp/"
-
-    download_music(artist, music)
-    lyrics = get_lyrics(artist, music, vagalume_api_key)
-
     replicate_client = replicate.Client(api_token=replicate_key)
-    model = replicate_client.models.get("stability-ai/stable-diffusion")
-    version = model.versions.get("db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf")
-    # generate_images(lyrics="A dog drinking a coffee\nA cat drinking a tea")
+    model = replicate_client.models.get(os.getenv('modelName'))
+    version = model.versions.get(os.getenv('modelIDVersion'))
+
+    # download_music(artist, music)
+    # lyrics = get_lyrics(artist, music, vagalume_api_key, first_phrase, qtd_phrases)
+    # generate_images(lyrics)
+    
